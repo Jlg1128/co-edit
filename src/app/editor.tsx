f727f8bd1ea2bbefac96bee82ee9c546eb28e405
Jlg11128 <799445385@qq.com>
2022-09-19 08:30:14 +0800

    add yjs

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -1,8 +1,13 @@
-import React, { useState, useCallback, useMemo, useEffect } from 'react';
+import React, { useState, useCallback, useMemo, useEffect, useImperativeHandle } from 'react';
 
 import { createEditor, Transforms, Editor, Text, Descendant, Element } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
-import './index.less';
+// Import the core binding
+import { withYjs, slateNodesToInsertDelta, YjsEditor } from '@slate-yjs/core';
+
+// Import yjs
+import * as Y from 'yjs';
+import { YText } from 'yjs/dist/src/internals';
 
 // Define a React component renderer for our code blocks.
 const CodeElement = (props: any) => (
@@ -20,6 +25,16 @@ const initialValue = [
 ];
 const MainEditor = () => {
   const [editor] = useState(() => withReact(createEditor()));
+  const sharedType = useMemo(() => {
+    const yDoc = new Y.Doc();
+    // Load the initial value into the yjs document
+    let actualSharedType = yDoc.get('content', Y.XmlText);
+    // @ts-ignore
+    console.log('actualSharedType', actualSharedType.applyDelta);
+    // @ts-ignore
+    actualSharedType.applyDelta(slateNodesToInsertDelta(initialValue));
+    return actualSharedType;
+  }, []);
 
   const renderElement = useCallback((props: any) => {
     switch (props.element.type) {
@@ -40,9 +55,6 @@ const MainEditor = () => {
     </span>
   );
 
-  useEffect(() => {
-  }, []);
-
   const renderLeaf = useCallback((props: any) => <Leaf {...props} />, []);
   return (
     <div>
@@ -56,59 +68,59 @@ const MainEditor = () => {
         点击
       </button>
       <Slate
-      editor={editor}
-      value={initialValue}
-      onChange={(value: Descendant[]) => {
-        const isAstChange = editor.operations.some(
-          (op: any) => op.type !== 'set_selection',
-        );
-        console.log('editor.operations', editor.operations);
-        console.log('value', value);
-        if (isAstChange) {
-          // Save the value to Local Storage.
-          const content = JSON.stringify(value);
-          localStorage.setItem('content', content);
-        }
-      }}
-      >
-        <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={(event: any) => {
-          if (!event.ctrlKey) {
-            return;
-          }
-          if (event.key === '`') {
-            event.preventDefault();
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: (n) => n.type === 'code',
-            });
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? 'paragraph' : 'code' },
-              { match: (n: any) => Editor.isBlock(editor, n) },
-            );
-          } else if (event.key === 'b') {
-            event.preventDefault();
-            const [match] = Editor.nodes(editor, {
-              match: (n) => Text.isText(n) && n.bold,
-            });
-            Transforms.setNodes(
-              editor,
-              { bold: !match },
-              { match: (n: any) => {
-                console.log('n', n.bold);
-                console.log('nentity', n);
-                console.log('🦊', Element.isElement(n));
-                console.log('block', Editor.isBlock(editor, n));
-                return Text.isText(n);
-              },
-              split: true },
-            );
+        editor={editor}
+        value={initialValue}
+        onChange={(value: Descendant[]) => {
+          const isAstChange = editor.operations.some(
+            (op: any) => op.type !== 'set_selection',
+          );
+          console.log('editor.operations', editor.operations);
+          console.log('value', value);
+          if (isAstChange) {
+            // Save the value to Local Storage.
+            const content = JSON.stringify(value);
+            localStorage.setItem('content', content);
           }
         }}
+      >
+        <Editable
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event: any) => {
+            if (!event.ctrlKey) {
+              return;
+            }
+            if (event.key === '`') {
+              event.preventDefault();
+              // Determine whether any of the currently selected blocks are code blocks.
+              const [match] = Editor.nodes(editor, {
+                match: (n) => n.type === 'code',
+              });
+              // Toggle the block type depending on whether there's already a match.
+              Transforms.setNodes(
+                editor,
+                { type: match ? 'paragraph' : 'code' },
+                { match: (n: any) => Editor.isBlock(editor, n) },
+              );
+            } else if (event.key === 'b') {
+              event.preventDefault();
+              const [match] = Editor.nodes(editor, {
+                match: (n) => Text.isText(n) && n.bold,
+              });
+              Transforms.setNodes(
+                editor,
+                { bold: !match },
+                { match: (n: any) => {
+                  console.log('n', n.bold);
+                  console.log('nentity', n);
+                  console.log('🦊', Element.isElement(n));
+                  console.log('block', Editor.isBlock(editor, n));
+                  return Text.isText(n);
+                },
+                split: true },
+              );
+            }
+          }}
        />
       </Slate>
     </div>
